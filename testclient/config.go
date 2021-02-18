7f0febb5ec67a8a75598bfaf22596389c6ea3d84
Krzesimir Nowak <krzesimir@kinvolk.io>
2021-02-18 22:18:20 +0100

    test client

---
+++ testclient/config.go
@@ -0,0 +1,47 @@
+package main
+
+import (
+	"encoding/json"
+	"errors"
+	"os"
+
+	"github.com/kinvolk/test-ds/internal"
+)
+
+type Config struct {
+	ServerPorts map[string]int `json:"server-ports"`
+}
+
+func NewConfig(cfgPath string) (Config, error) {
+	cfgFile, err := os.Open(cfgPath)
+	if err != nil {
+		return Config{}, err
+	}
+	defer cfgFile.Close()
+
+	cfg := Config{}
+	if err := json.NewDecoder(cfgFile).Decode(&cfg); err != nil {
+		return Config{}, err
+	}
+
+	if err := cfg.Validate(); err != nil {
+		return Config{}, err
+	}
+
+	return cfg, nil
+}
+
+func (cfg Config) Validate() error {
+	if len(cfg.ServerPorts) == 0 {
+		return errors.New("No server ports specified")
+	}
+	for name, port := range cfg.ServerPorts {
+		if name == "" {
+			return errors.New("Empty server name")
+		}
+		if err := internal.ValidatePort(port, name); err != nil {
+			return err
+		}
+	}
+	return nil
+}
---
+++ testclient/main.go
@@ -0,0 +1,59 @@
+package main
+
+import (
+	"encoding/hex"
+	"errors"
+	"flag"
+	"fmt"
+	"io"
+	"net"
+	"net/http"
+	"os"
+	"strconv"
+	"strings"
+)
+
+func main() {
+	if err := progMain(); err != nil {
+		fmt.Fprintf(os.Stderr, "error: %v\n", err)
+		os.Exit(1)
+	}
+}
+
+func progMain() error {
+	cfgPath := ""
+	flag.StringVar(&cfgPath, "config", "", "path to json config")
+	flag.Parse()
+	if len(flag.Args()) != 2 {
+		return errors.New("expected two parameters, for the server name and message to send")
+	}
+	serverName := flag.Arg(0)
+	if serverName == "" {
+		return errors.New("the server parameter can not be empty")
+	}
+	message := flag.Arg(1)
+	if message == "" {
+		return errors.New("message can not be empty")
+	}
+
+	cfg, err := NewConfig(cfgPath)
+	if err != nil {
+		return err
+	}
+
+	port, ok := cfg.ServerPorts[serverName]
+	if !ok {
+		return fmt.Errorf("unknown server %s", serverName)
+	}
+
+	address := net.JoinHostPort("localhost", strconv.Itoa(port))
+	messageReader := strings.NewReader(message)
+	response, err := http.Post(address, "application/octet-stream", messageReader)
+	if err != nil {
+		return err
+	}
+	defer response.Body.Close()
+	encoder := hex.NewEncoder(os.Stdout)
+	_, _ = io.Copy(encoder, response.Body)
+	return nil
+}
